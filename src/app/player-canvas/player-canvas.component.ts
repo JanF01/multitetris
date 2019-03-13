ff462069565c110246c239c3c11784cb24954f28
Jan Fiołka <jan.fiolka44@gmail.com>
2019-03-13 23:56:37 +0100

    Developing the Tetris System day 3 - 13 marca 2019 - Basic Tetris - DONE

--- src/app/player-canvas/player-canvas.component.ts
+++ src/app/player-canvas/player-canvas.component.ts
@@ -11,35 +11,40 @@ export class PlayerCanvasComponent implements AfterViewInit, OnInit {
    @ViewChild('canvas') public canvas: ElementRef;
 
 
-   @Input() private width = 600;
-   @Input() private height = 880;
+   @Input() private width = 420;
+   @Input() private height = 770;
 
 
-   private x = 15;
+   private x = 12;
    private y = 22;
    private grid = [];
-   private blockSize = 40;
+   private blockSize = 35;
    private blockAmount = 0;
    private current = [];
    private shapes = [
-      [1,1,1,1,
-       0,0,0,0],
-      [1,1,0,0,
-        1,1,0,0],
-      [0,1,0,0,
-        1,1,1,0],
-      [1,0,0,0,
-          1,1,1,0],
-      [1,0,0,0,
-            1,0,0,0,
-            1,1,0,0],
+      [0,0,0,0,
+       1,1,1,1],
+      [1,1,0,
+       1,1,0],
+      [0,1,0,
+        1,1,1],
+
+      [1,1,0,
+      0,1,1],
+      [0,1,1,
+        1,1,0],
+      [1,0,0,
+       1,1,1],
+       [0,0,1,
+        1,1,1],
 
    ];
    private end: boolean = false;
-
+   private s = 4;
    private currentX: number = 0;
    private currentY: number = 0;
    private currentType: number = 0;
+   private blockColor: string = "";
    private gameInterval;
 
   private subscription: Subscription;
@@ -83,24 +88,54 @@ export class PlayerCanvasComponent implements AfterViewInit, OnInit {
      this.newBlock();
 
 
-    this.gameInterval = setInterval(()=>this.down(),200);
+    this.gameInterval = setInterval(()=>this.down(),350);
 
    }
    
 
    newBlock(){
-     this.currentX = Math.floor(Math.random()*12);
+    
      this.currentY = 0;
-     this.currentType = Math.floor(Math.random()*5);
+     this.currentX = 4;
+     this.currentType = Math.floor(Math.random()*7);
+     if(this.currentType==0){
+      this.s = 4;
+      this.blockColor = "#0071A0";
+     }
+     else{
+       this.s = 3;
+     }
+
+
+
+ 
+    if(this.currentType == 1){
+      this.blockColor = "#A07100";
+    }
+    else if(this.currentType == 2){
+      this.blockColor = "#0021F0";
+    }
+    else if(this.currentType == 3){
+      this.blockColor = "#802130";
+    }
+    else if(this.currentType == 4){
+      this.blockColor = "#F0F180";
+    }
+    else if(this.currentType == 5){
+      this.blockColor = "#80F1F0";
+    }
+    else if(this.currentType == 6){
+      this.blockColor = "#F080F1";
+    }
 
      this.current = this.shapes[this.currentType];
-
+   
 
      let shape = [];
-    for(let y=0;y<4;y++){
+    for(let y=0;y<this.s;y++){
      shape[y] = [];
-     for(let x=0;x<4;x++){
-        let grid = y * 4 + x;
+     for(let x=0;x<this.s;x++){
+        let grid = y * this.s + x;
         if(typeof this.current[grid] !== 'undefined' && this.current[grid] ){
              shape[y][x] = 1;
         }
@@ -117,7 +152,7 @@ export class PlayerCanvasComponent implements AfterViewInit, OnInit {
       for(let i=0;i<this.y;i++){
          let xLine = [];
          for(let y=0;y<this.x;y++){
-         xLine.push(0);
+         xLine.push([0,"#151515"]);
          }
          this.grid.push(xLine);
       }
@@ -149,58 +184,137 @@ export class PlayerCanvasComponent implements AfterViewInit, OnInit {
    keyPress(event: KeyboardEvent){
 
      switch (event.code){
-    case 'ArrowLeft':{
+       case 'ArrowLeft':{
      if(this.checkAround(-1)){
        this.currentX -= 1;
        this.drawAll();
      }
+     break;
     }
-       break;
        case 'ArrowRight':{
        if(this.checkAround(1)){
         this.currentX += 1;
         this.drawAll();
       }
+      break;
        }
-         break;
-           case 'ArrowDown':
+       case 'ArrowDown':{
            this.down();
-       
-             break;
-     }
+           break; 
+           }
+          case 'ArrowUp':{
+            if(this.currentType!=1){
+            let rotated = this.rotation(this.current);
+            if(this.checkAround(0,0,rotated)){
+             this.current = rotated;
+              this.down();
+            }
+          }
+            break; 
+          }
+      }
      this.drawAll();
 
    }
 
 
+   rotation(current){
+        let rotated = [];
+    for(let y=0;y<this.s;y++){
+      rotated[y] = [];
+     for(let x=0;x<this.s;x++){
+       rotated[y][x] = current[this.s-1-x][y];
+     }
+
+    }
+      return rotated;
+   }
 
 
    blockStop(){
-    for(let y=0;y<4;y++){
-      for(let x=0;x<4;x++){
+    for(let y=0;y<this.s;y++){
+      for(let x=0;x<this.s;x++){
         if(this.current[y][x]){
-          this.grid[y+ this.currentY][x+ this.currentX] = this.current[y][x];
+          this.grid[y+ this.currentY][x+ this.currentX][0] = this.current[y][x];
+          this.grid[y+ this.currentY][x+ this.currentX][1] = this.blockColor;
+      
         }
 
       }
     }
 
+     this.checkLines();
    }
 
+  checkLines(){
+
+    for(let i=this.currentY+this.s-1;i>=this.currentY;i--){
+     this.checkForLine(i);
+
+    }
+  }
+
+  checkForLine(i){
+    if(i<this.y){
+      let line = true;
+       for(let x=0;x<this.x;x++){
+           if(this.grid[i][x][0]==0){
+             line = false;
+           }
+          }
+       if(line){
+         this.destroyLine(i); 
+         this.checkForLine(i);
+       }
+       else{
+       return false;
+       }
   
+  }
+}
+
+  destroyLine(y){
+     for(let x=0;x<this.x;x++){
+         this.grid[y][x][0] = 0;
+         this.grid[y][x][1] = "#151515";
+     }
+
+     let newGrid = [];
+     
+     for(let o=y;o>=1;o--){
+       newGrid[o] = [];
+        for(let x=0;x<this.x;x++){
+       newGrid[o][x] = [this.grid[o-1][x][0],this.grid[o-1][x][1]];
+        }
+     }
+     newGrid[0] = [];
+     for(let i=0;i<this.x;i++){
+         newGrid[0][i] = [0,"#151515"];
+     }
+
+     for(let j=y+1;j<this.y;j++){
+       newGrid[j] = [];
+       for(let x=0;x<this.x;x++){
+         newGrid[j][x] = [this.grid[j][x][0],this.grid[j][x][1]];
+       }
+
+     }
+     this.grid = newGrid;
+
+  }
 
   checkAround(xPos = 0,yPos = 0,current = this.current){
   
    xPos += this.currentX;
    yPos += this.currentY;
 
-   for(let y=0;y<4;y++){
+   for(let y=0;y<this.s;y++){
 
-    for(let x=0;x<4;x++){
+    for(let x=0;x<this.s;x++){
        if(current[y][x]){
           if(typeof this.grid[y + yPos] === "undefined"
           || typeof this.grid[y + yPos][x + xPos] === "undefined"
-          || this.grid[y + yPos][x + xPos]
+          || this.grid[y + yPos][x + xPos][0]
           || x + xPos < 0
           || x + xPos >= this.x
           || y + yPos >= this.y){
@@ -221,32 +335,20 @@ export class PlayerCanvasComponent implements AfterViewInit, OnInit {
 
  
   drawAll(){
-    this.context.fillStyle = "#000000";
+    this.context.fillStyle = "#000";
     this.context.fillRect(0,0,this.width,this.height);
-    this.context.fillStyle = "#70A100";
+   
     for(let i = 0;i < this.grid.length;i++){
       for(let j =0;j<this.grid[i].length;j++){
-        if(this.grid[i][j]) this.context.fillRect(j*this.blockSize,i*this.blockSize,this.blockSize-1,this.blockSize-1);
+       this.context.fillStyle =  this.grid[i][j][1]; 
+       this.context.fillRect(j*this.blockSize,i*this.blockSize,this.blockSize-1,this.blockSize-1);
+
       }
     }
 
-    if(this.currentType == 0){
-      this.context.fillStyle = "#0071A0";
-    }
-    else if(this.currentType == 1){
-      this.context.fillStyle = "#A07100";
-    }
-    else if(this.currentType == 2){
-      this.context.fillStyle = "#0021F0";
-    }
-    else if(this.currentType == 3){
-      this.context.fillStyle = "#802130";
-    }
-    else{
-      this.context.fillStyle = "#80F1F0";
-    }
-   for(let y=0;y<4;y++){
-      for(let x=0;x<4;x++){
+    this.context.fillStyle = this.blockColor;
+   for(let y=0;y<this.s;y++){
+      for(let x=0;x<this.s;x++){
          if(this.current[y][x]){
              this.context.fillRect((this.currentX + x)*this.blockSize,(this.currentY + y)*this.blockSize,this.blockSize-1,this.blockSize-1);
          }
